6d9bdec160fe1aa7988f98a9f51ce3597ce64309
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-09 19:30:20 +0200

    Draw grid

--- internal/render.go
+++ internal/render.go
@@ -7,20 +7,39 @@ import (
 )
 
 func render(world *World) {
-	camera := rl.Camera2D{}
-	camera.Target = world.position[world.player]
-	camera.Offset = util.GetHalfScreen()
-	camera.Zoom = 8
 
 	rl.BeginDrawing()
 	defer rl.EndDrawing()
 
 	rl.ClearBackground(rl.Black)
-	rl.BeginMode2D(camera)
+
+	renderGrid()
+
+	rl.BeginMode2D(createCamera(world))
 	renderPlayer(world)
 	renderEnemies(world)
 	renderProjectiles(world)
 	rl.EndMode2D()
+}
+
+func renderGrid() {
+	for i := -25; i <= 25; i++ {
+		spacing := 1000 * int32(i)
+		rl.DrawLine(-9999, spacing, 9999, spacing, rl.RayWhite)
+		rl.DrawLine(spacing, -9999, spacing, 9999, rl.RayWhite)
+	}
+}
+
+func createCamera(world *World) rl.Camera2D {
+	camera := rl.Camera2D{
+		Target: world.position[world.player],
+		Offset: util.GetHalfScreen(),
+		Zoom:   8,
+	}
+	camera.Target = world.position[world.player]
+	camera.Offset = util.GetHalfScreen()
+	camera.Zoom = 8
+	return camera
 }
 
 func renderPlayer(world *World) {
