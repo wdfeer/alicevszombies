599746dc35424e0c6fd3eaa7cbf5e71cd747dd53
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-13 18:59:50 +0200

    Update spell controls

--- internal/spells.go
+++ internal/spells.go
@@ -3,17 +3,17 @@ package internal
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func updateSpells(world *World) {
-	if world.playerData.mana >= 10 {
-		if rl.IsKeyPressed(rl.KeyOne) {
-			id := newDoll(world)
-			world.position[id] = world.position[world.player]
-			world.playerData.mana -= 10
-		} else if rl.IsKeyPressed(rl.KeyTwo) {
-			heal(world, world.player, 5)
-			world.playerData.mana -= 10
-		} else if rl.IsKeyPressed(rl.KeyThree) {
-			world.playerData.dollDamage += 0.25
-			world.playerData.mana -= 10
-		}
+	if world.playerData.mana >= 5 && (rl.IsKeyPressed(rl.KeyOne) || rl.IsKeyPressed(rl.KeyH)) {
+		heal(world, world.player, 5)
+		world.playerData.mana -= 5
+	}
+	if world.playerData.mana >= 10 && (rl.IsKeyPressed(rl.KeyTwo) || rl.IsKeyPressed(rl.KeyJ)) {
+		id := newDoll(world)
+		world.position[id] = world.position[world.player]
+		world.playerData.mana -= 10
+	}
+	if world.playerData.mana >= 10 && (rl.IsKeyPressed(rl.KeyThree) || rl.IsKeyPressed(rl.KeyK)) {
+		world.playerData.dollDamage += 0.25
+		world.playerData.mana -= 10
 	}
 }
