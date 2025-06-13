0127eb47492ec810590d3eb4a8b574b2a7cb3879
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-13 10:44:30 +0200

    Draw wave counter

--- internal/ui.go
+++ internal/ui.go
@@ -10,8 +10,17 @@ import (
 func renderUI(world *World) {
 	rl.DrawTextureEx(assets.Textures["cursor"], rl.GetMousePosition(), 0, 4, rl.White)
 
-	str := "HP: " + fmt.Sprint(world.hp[world.player].val)
-	center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: float32(rl.GetScreenHeight())/2 + 200}
-	pos := util.CenterText(str, 32, center)
-	rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
+	{ // Wave counter
+		str := "Wave " + fmt.Sprint(world.enemySpawner.wave)
+		center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: float32(rl.GetScreenHeight())/2 - 200}
+		pos := util.CenterText(str, 32, center)
+		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
+	}
+
+	{ // Hp bar
+		str := "HP: " + fmt.Sprint(world.hp[world.player].val)
+		center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: float32(rl.GetScreenHeight())/2 + 200}
+		pos := util.CenterText(str, 32, center)
+		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
+	}
 }
