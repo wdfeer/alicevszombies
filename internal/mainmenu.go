d8e85e95acc633126654654c336acdc7d0e1a6ce
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-17 06:25:34 +0200

    Create MainMenu

--- internal/mainmenu.go
+++ internal/mainmenu.go
@@ -7,7 +7,7 @@ import (
 )
 
 func updateMainMenu(world *World) {
-	if rl.IsKeyPressed(rl.KeyOne) {
+	if rl.IsKeyPressed(rl.KeyJ) {
 		world.paused = false
 		world.uistate.isMainMenu = false
 	}
@@ -16,5 +16,10 @@ func updateMainMenu(world *World) {
 func renderMainMenu() {
 	rl.DrawRectangle(0, 0, int32(rl.GetScreenWidth()), int32(rl.GetScreenHeight()), rl.ColorAlpha(rl.Black, 0.9))
 	util.DrawTextCenteredSpaced("alicevszombies", 80, rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: 120}, 16)
-	// TODO: render title and start text, key press tooltip
+
+	halfHeight := int32(rl.GetScreenHeight() / 2)
+	rl.DrawText("Start", 160, halfHeight-50, 64, rl.White)
+	rl.DrawText("J", 400, halfHeight-50, 60, rl.White)
+	rl.DrawText("Exit", 160, halfHeight+50, 64, rl.White)
+	rl.DrawText("DEL", 400, halfHeight+50, 60, rl.White)
 }
