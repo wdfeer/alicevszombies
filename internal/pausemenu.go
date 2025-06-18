abd933df9705f9b569674989d644ce15af31c70d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-18 12:23:13 +0200

    Convert pause menu to raygui

--- internal/pausemenu.go
+++ internal/pausemenu.go
@@ -3,16 +3,31 @@ package internal
 import (
 	"alicevszombies/internal/util"
 
+	"github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func renderPauseMenu(world *World) {
 	rl.DrawRectangle(0, 0, int32(rl.GetScreenWidth()), int32(rl.GetScreenHeight()), rl.ColorAlpha(rl.Black, 0.4))
 
-	pos := util.HalfScreenSize()
-	util.DrawTextCenteredSpaced("Paused", 256, pos, 16)
-	pos.Y += 128
-	util.DrawTextCenteredSpaced("ESC = Resume", 64, pos, 4)
-	pos.Y += 64
-	util.DrawTextCenteredSpaced("DEL = Quit", 64, pos, 4)
+	center := util.HalfScreenSize()
+	util.DrawTextCenteredSpaced("Paused", 256, center, 16)
+
+	buttonWidth := float32(400)
+	buttonHeight := float32(120)
+	buttonSpacing := float32(40)
+
+	startY := center.Y + 128
+	centerX := center.X - buttonWidth/2
+
+	resumeRect := rl.Rectangle{X: centerX, Y: startY, Width: buttonWidth, Height: buttonHeight}
+	quitRect := rl.Rectangle{X: centerX, Y: startY + buttonHeight + buttonSpacing, Width: buttonWidth, Height: buttonHeight}
+
+	if raygui.Button(resumeRect, "Resume") {
+		world.paused = false
+	}
+
+	if raygui.Button(quitRect, "Quit") {
+		rl.CloseWindow()
+	}
 }
