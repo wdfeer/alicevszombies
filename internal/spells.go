94ed99b7dd3e251661de43879bad481644ba1565
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-21 17:36:12 +0200

    Disable spell buttons if low mana

--- internal/spells.go
+++ internal/spells.go
@@ -12,12 +12,20 @@ func renderSpells(world *World) {
 	size := rl.Vector2{X: 200, Y: 80}
 	center := rl.Vector2{X: 300, Y: halfHeight - size.Y*1.2}
 
+	if world.playerData.mana < 5 {
+		raygui.Disable()
+	}
+
 	if raygui.Button(util.CenterRectangle(center, size), "") && world.playerData.mana >= 5 {
 		heal(world, world.player, 5)
 		world.playerData.mana -= 5
 	}
 	util.DrawTextureCenteredScaled(assets.textures["heal_icon"], rl.Vector2{X: center.X - size.X/5, Y: center.Y}, 4)
 	util.DrawTextCentered("H", 40, rl.Vector2{X: center.X + size.X/5, Y: center.Y})
+
+	if world.playerData.mana < 10 {
+		raygui.Disable()
+	}
 
 	center.Y += size.Y * 1.2
 	if raygui.Button(util.CenterRectangle(center, size), "") && world.playerData.mana >= 10 {
@@ -36,4 +44,6 @@ func renderSpells(world *World) {
 	}
 	util.DrawTextureCenteredScaled(assets.textures["pitem_icon"], rl.Vector2{X: center.X - size.X/5, Y: center.Y}, 4)
 	util.DrawTextCentered("K", 40, rl.Vector2{X: center.X + size.X/5, Y: center.Y})
+
+	raygui.Enable()
 }
