0a3aa6b4cc79e55e0d760e4ea5f097967834b67f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-25 13:01:52 +0200

    Create uiscale helper functions

--- internal/ui/uiscale.go
+++ internal/ui/uiscale.go
@@ -7,3 +7,11 @@ var UIScale float32 = 1
 func UpdateUIScale() {
 	UIScale = float32(rl.GetScreenHeight()) / 1440
 }
+
+func ScaleV(vec rl.Vector2) rl.Vector2 {
+	return rl.Vector2Scale(vec, UIScale)
+}
+
+func ScaleR(rect rl.Rectangle) rl.Rectangle {
+	return rl.Rectangle{X: rect.X * UIScale, Y: rect.Y * UIScale, Width: rect.Width * UIScale, Height: rect.Height * UIScale}
+}
