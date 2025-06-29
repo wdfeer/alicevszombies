423216bf5e2a74441bd435486f37ca6826c4cbb1
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-29 19:32:27 +0200

    Create colors.go

---
+++ internal/colors/colors.go
@@ -0,0 +1,9 @@
+package colors
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+var (
+	Yellow rl.Color = rl.NewColor(251, 242, 54, 255)
+	Blue   rl.Color = rl.NewColor(98, 154, 255, 255)
+	Red    rl.Color = rl.NewColor(216, 86, 98, 255)
+)
