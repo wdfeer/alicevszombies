dfc4c02f7877edcadeb5f8648f231461e4124b4e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-29 12:51:50 +0200

    Reduce purple bullet damage

--- internal/projectile.go
+++ internal/projectile.go
@@ -68,7 +68,7 @@ var projectileTypes = struct {
 	},
 	purpleBullet: ProjectileType{
 		hostile: true,
-		damage:  5,
+		damage:  3,
 		texture: "purple_bullet",
 		size:    rl.Vector2{X: 4, Y: 4},
 	},
