cd41b4fd241cba7a27c1aaae960599d782a0b2f4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-29 12:11:54 +0200

    Implement poison update

--- internal/status.go
+++ internal/status.go
@@ -1,5 +1,7 @@
 package internal
 
+import "alicevszombies/internal/util"
+
 type Status struct {
 	poison float32
 	// TODO: implement slow/weak for blue stuff
@@ -7,6 +9,13 @@ type Status struct {
 
 func updateStatus(world *World) {
 	for id, status := range world.status {
-		// TODO: poison deals damage and reduces duration
+		if status.poison > 0 {
+			if util.ModF(status.poison, 1) < dt {
+				damage(world, id, 1)
+			}
+			world.status[id] = Status{
+				poison: status.poison - dt,
+			}
+		}
 	}
 }
