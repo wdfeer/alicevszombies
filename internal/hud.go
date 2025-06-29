54422dea545f262502777b6d77c644aa1111e067
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-29 19:28:15 +0200

    Create a progress bar at the bottom if boss

--- internal/hud.go
+++ internal/hud.go
@@ -4,30 +4,34 @@ import (
 	"alicevszombies/internal/util"
 	"fmt"
 
+	"github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func renderHUD(world *World) {
-	// Top
+	size := util.ScreenSize()
+	halfSize := util.HalfScreenSize()
+
+	// Upper Center
 	{ // Wave counter
 		str := "Wave " + fmt.Sprint(world.enemySpawner.wave)
-		center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: 200}
+		center := rl.Vector2{X: halfSize.X, Y: 200}
 		pos := util.CenterText(str, 32, center)
 		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
 	}
 
-	// Bottom
+	// Lower Center
 	yPositions := util.SpaceCentered(50, 4, float32(rl.GetScreenHeight())-200)
 	{ // HP bar
 		str := "HP: " + fmt.Sprint(world.hp[world.player].val)
-		center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: yPositions[0]}
+		center := rl.Vector2{X: halfSize.X, Y: yPositions[0]}
 		pos := util.CenterText(str, 32, center)
 		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
 	}
 
 	{ // MP bar
 		str := "MP: " + fmt.Sprint(world.playerData.mana)
-		center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: yPositions[1]}
+		center := rl.Vector2{X: halfSize.X, Y: yPositions[1]}
 		pos := util.CenterText(str, 32, center)
 		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
 	}
@@ -35,15 +39,30 @@ func renderHUD(world *World) {
 	// Statuses
 	if world.status[world.player].poison > 0 {
 		str := "Poisoned"
-		center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: yPositions[2]}
+		center := rl.Vector2{X: halfSize.X, Y: yPositions[2]}
 		pos := util.CenterText(str, 32, center)
 		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.Purple)
 	}
 	if world.status[world.player].slow > 0 {
 		str := "Slowed"
-		center := rl.Vector2{X: float32(rl.GetScreenWidth()) / 2, Y: yPositions[3]}
+		center := rl.Vector2{X: halfSize.X, Y: yPositions[3]}
 		pos := util.CenterText(str, 32, center)
 		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.Blue)
+	}
+
+	// Boss Bar
+	{
+		boss := -1
+		for id, typ := range world.enemy {
+			if typ.spawnData.boss {
+				boss = int(id)
+				break
+			}
+		}
+		if boss != -1 {
+			hp := world.hp[Entity(boss)]
+			raygui.ProgressBar(rl.NewRectangle(0, size.Y-16, size.X, 16), "", "", hp.val, 0, 100)
+		}
 	}
 
 	renderSpells(world)
