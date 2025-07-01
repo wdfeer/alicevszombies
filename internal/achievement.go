91172d56117adf2515212633166a4f386930ef77
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-01 07:35:45 +0200

    Create wave 100 achievement

--- internal/achievement.go
+++ internal/achievement.go
@@ -1,9 +1,25 @@
 package internal
 
-type Achievements struct {
-	// TODO: add achievements
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+type Achievements = map[*AchievementType]float32
+
+type AchievementType struct {
+	name        string
+	description string
 }
 
+var (
+	Wave100Reached = AchievementType{
+		name:        "Overachiever",
+		description: "Reach wave 100",
+	}
+)
+
 func updateAchievements(world *World) {
-	// TODO: update the achievements
+	stats.Achievements[&Wave100Reached] = float32(stats.HighestWave[UNDEFINED]) / 100
+}
+
+func renderAchievements(origin rl.Vector2) {
+	// TODO: render achievement panel
 }
