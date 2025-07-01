b5c5594e843c7252dd71686325a442260b003117
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-01 07:57:57 +0200

    Draw basic panel for each achievement

--- internal/achievement.go
+++ internal/achievement.go
@@ -30,15 +30,24 @@ func updateAchievements(world *World) {
 func renderAchievements(origin rl.Vector2) {
 	size := rl.Vector2{X: 480, Y: 120}
 	spacing := float32(40)
-	panelSize := rl.Vector2{X: size.X, Y: size.Y*4 + spacing*5}
+	count := len(stats.Achievements)
+	panelSize := rl.Vector2{X: size.X + spacing*2, Y: size.Y*float32(count) + spacing*(float32(count)+1)}
 
 	oldFontsize := raygui.GetStyle(raygui.DEFAULT, raygui.TEXT_SIZE)
 
 	raygui.Panel(util.RectangleV(origin, panelSize), "")
 
-	raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SIZE, 32)
+	x := origin.X + spacing
+	for id, _ := range stats.Achievements {
+		// TODO: get name/description by id for displaying it
+		name := "TODO"
+		description := "TODO"
 
-	// TODO: display achievements
+		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SIZE, 32)
+		raygui.Label(rl.Rectangle{X: x, Y: origin.Y + (float32(id)+1)*(size.Y+spacing), Width: size.X, Height: size.Y / 2}, name)
+		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SIZE, 24)
+		raygui.Label(rl.Rectangle{X: x, Y: origin.Y + (float32(id)+1)*(size.Y+spacing) + size.Y/2, Width: size.X, Height: size.Y / 2}, description)
+	}
 
 	raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SIZE, oldFontsize)
 }
