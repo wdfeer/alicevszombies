a9a7b7a8fa9c47c4852eb805d34b52fc8abb0228
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-03 08:56:40 +0200

    Display doll costs in upgrade screen

--- internal/upgradescreen.go
+++ internal/upgradescreen.go
@@ -32,7 +32,8 @@ func renderUpgradeScreen(world *World) {
 
 	screen := world.uistate.upgradeScreen
 
-	width := float32(440)
+	const width = float32(440)
+	const height = float32(240)
 
 	center := util.HalfScreenSize()
 	xPositions := util.SpaceCentered(width+120, len(screen.upgrades), center.X-width/2)
@@ -44,11 +45,47 @@ func renderUpgradeScreen(world *World) {
 		2: rl.KeyThree,
 	}
 	upgrade := -1
-	for i := range len(screen.upgrades) {
-		rect := rl.NewRectangle(xPositions[i], center.Y-64, width, 128)
+	for i, up := range screen.upgrades {
+		rect := rl.Rectangle{X: xPositions[i], Y: center.Y - height/2, Width: width, Height: height}
 		raygui.Panel(rect, "")
-		raygui.Label(rect, world.uistate.upgradeScreen.upgrades[i].name)
-		rect.Y += 144
+
+		titleRect := rect
+		if up.cost == nil {
+			titleRect.Y -= height / 8
+		} else {
+			titleRect.Y -= height / 3
+		}
+		raygui.Label(titleRect, world.uistate.upgradeScreen.upgrades[i].name)
+
+		if up.cost != nil {
+			dollCostRect := rect
+			dollCostRect.X += 16
+			dollCostRect.Width -= 32
+			dollCostRect.Y += height / 8
+			raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_ALIGNMENT, int64(raygui.TEXT_ALIGN_LEFT))
+			raygui.Label(dollCostRect, "Cost:")
+			raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_ALIGNMENT, int64(raygui.TEXT_ALIGN_CENTER))
+
+			const dollScale = 4
+			const dollSpacing = 4
+			totalDollCostWidth := float32(0)
+			maxHeight := int32(0)
+			for dollT, count := range up.cost {
+				totalDollCostWidth += float32(assets.textures[dollT.texture].Width*dollScale+dollSpacing) * float32(count)
+				if assets.textures[dollT.texture].Height > maxHeight {
+					maxHeight = assets.textures[dollT.texture].Height
+				}
+			}
+			pos := util.CenterSomething(totalDollCostWidth, float32(maxHeight), rl.Vector2{X: xPositions[i] + width/2, Y: dollCostRect.Y + dollCostRect.Height/2 - float32(maxHeight)*dollScale/2})
+			for dollT, count := range up.cost {
+				for range count {
+					rl.DrawTextureEx(assets.textures[dollT.texture], pos, 0, float32(dollScale), rl.White)
+					pos.X += float32(assets.textures[dollT.texture].Width + dollSpacing)
+				}
+			}
+		}
+
+		rect.Y += height + 48
 		rect.Height = 64
 		if raygui.Button(rect, fmt.Sprint(i+1)) || rl.IsKeyPressed(keys[i]) {
 			upgrade = i
