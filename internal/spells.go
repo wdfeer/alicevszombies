2c43ef686f9419496c3342fc3700ea8157f8ba93
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-12 08:41:40 +0200

    Change spell binds to 1-4

--- internal/spells.go
+++ internal/spells.go
@@ -23,35 +23,35 @@ func renderSpells(world *World) {
 	if !canHeal {
 		raygui.Disable()
 	}
-	if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyH)) && canHeal && !world.paused {
+	if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyOne)) && canHeal && !world.paused {
 		heal(world, world.player, 5)
 		world.playerData.mana -= 5
 	}
 	raygui.Enable()
 	util.DrawTextureCenteredScaled(assets.textures["heal_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-	util.DrawTextCentered("H", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+	util.DrawTextCentered("1", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 
 	if world.playerData.mana < 10 {
 		raygui.Disable()
 	}
 
 	pos.Y = yPositions[1]
-	if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyJ)) && world.playerData.mana >= 10 && !world.paused {
+	if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyTwo)) && world.playerData.mana >= 10 && !world.paused {
 		id := newDoll(world, &dollTypes.basicDoll)
 		world.position[id] = world.position[world.player]
 		world.playerData.mana -= 10
 	}
 	util.DrawTextureCenteredScaled(assets.textures["doll_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-	util.DrawTextCentered("J", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+	util.DrawTextCentered("2", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 
 	pos.Y = yPositions[2]
-	if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyK)) && world.playerData.mana >= 10 && !world.paused {
+	if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyThree)) && world.playerData.mana >= 10 && !world.paused {
 		world.paused = true
 		world.playerData.mana -= 10
 		newUpgradeScreen(world)
 	}
 	util.DrawTextureCenteredScaled(assets.textures["pitem_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-	util.DrawTextCentered("K", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+	util.DrawTextCentered("3", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 
 	if spellCount == 4 {
 		disable := world.playerData.mana < 100 || len(availableUniqueUpgrades(world)) == 0
@@ -59,13 +59,13 @@ func renderSpells(world *World) {
 			raygui.Disable()
 		}
 		pos.Y = yPositions[3]
-		if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyL)) && !disable && !world.paused {
+		if (raygui.Button(util.CenterRectangle(pos, size), "") || rl.IsKeyPressed(rl.KeyFour)) && !disable && !world.paused {
 			world.paused = true
 			world.playerData.mana -= 100
 			newUniqueUpgradeScreen(world)
 		}
 		util.DrawTextureCenteredScaled(assets.textures["unique_upgrade_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-		util.DrawTextCentered("L", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+		util.DrawTextCentered("4", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 	}
 
 	raygui.Enable()
