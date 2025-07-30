8f19a1f772f292d5096d8ee2e77dc25bc8766787
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-30 15:42:49 +0200

    Implement UI Scale spinner functionality

--- internal/uiscale.go
+++ internal/uiscale.go
@@ -11,22 +11,34 @@ var textSize64 int64 = 64
 var textSize40 int64 = 40
 
 func updateUIScale() {
-	height := rl.GetScreenHeight()
-	if height > 1080 {
+	scaleType := options.UIScale
+	if scaleType == 0 {
+		height := rl.GetScreenHeight()
+		if height > 1080 {
+			scaleType = 1
+		} else if height > 720 {
+			scaleType = 2
+		} else {
+			scaleType = 3
+		}
+	}
+
+	switch scaleType {
+	case 1:
 		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SIZE, 80)
 		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SPACING, 8)
 		textSize256 = 256
 		textSize64 = 64
 		textSize40 = 40
 		uiScale = 1
-	} else if height > 720 {
+	case 2:
 		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SIZE, 64)
 		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SPACING, 6)
 		textSize256 = 160
 		textSize64 = 48
 		textSize40 = 32
 		uiScale = 64. / 80
-	} else {
+	case 3:
 		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SIZE, 32)
 		raygui.SetStyle(raygui.DEFAULT, raygui.TEXT_SPACING, 4)
 		textSize256 = 80
