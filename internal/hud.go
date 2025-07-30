b798a21fec585e0f003f3510b0c2aa5cbc652cba
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-30 15:35:40 +0200

    Implement HUD ui scale

--- internal/hud.go
+++ internal/hud.go
@@ -16,62 +16,62 @@ func renderHUD(world *World) {
 
 	{ // Wave counter
 		str := "Wave " + fmt.Sprint(world.enemySpawner.wave)
-		center := rl.Vector2{X: halfSize.X, Y: 200}
-		pos := util.CenterText(str, 32, center)
-		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
+		center := rl.Vector2{X: halfSize.X, Y: 200 * uiScale}
+		pos := util.CenterText(str, float32(textSize40), center)
+		rl.DrawText(str, int32(pos.X), int32(pos.Y), int32(textSize40), rl.White)
 	}
 
 	// Lower Center
-	yPositions := util.SpaceCentered(50, 6, float32(rl.GetScreenHeight())-200)
+	yPositions := util.SpaceCentered(50*uiScale, 6, float32(rl.GetScreenHeight())-200*uiScale)
 
 	// Stamina bar
 	if world.playerData.stamina < 1 {
-		width := float32(200) * world.playerData.stamina
+		width := float32(200) * uiScale * world.playerData.stamina
 		pos := rl.Vector2{X: halfSize.X - width/2, Y: yPositions[0]}
-		size := rl.Vector2{X: width, Y: 8}
+		size := rl.Vector2{X: width, Y: 8 * uiScale}
 		rl.DrawRectangleV(pos, size, colors.Yellow)
 	}
 
 	{ // HP
 		str := "HP: " + fmt.Sprint(world.hp[world.player].val)
 		center := rl.Vector2{X: halfSize.X, Y: yPositions[1]}
-		pos := util.CenterText(str, 32, center)
+		pos := util.CenterText(str, float32(textSize40), center)
 		if world.hp[world.player].val <= 5 {
-			pos := util.CenterText(str, 34, center)
-			rl.DrawTextEx(rl.GetFontDefault(), str, pos, 34, 2, colors.Red)
+			pos := util.CenterText(str, float32(textSize40)+2, center)
+			rl.DrawTextEx(rl.GetFontDefault(), str, pos, float32(textSize40)+2, 2, colors.Red)
 		}
-		rl.DrawTextEx(rl.GetFontDefault(), str, pos, 32, 2, rl.White)
+		rl.DrawTextEx(rl.GetFontDefault(), str, pos, float32(textSize40), 2, rl.White)
 	}
 
 	{ // MP
 		str := "MP: " + fmt.Sprint(world.playerData.mana)
 		center := rl.Vector2{X: halfSize.X, Y: yPositions[2]}
-		pos := util.CenterText(str, 32, center)
+		pos := util.CenterText(str, float32(textSize40), center)
 		if world.playerData.mana >= 100 {
-			pos := util.CenterText(str, 34, center)
-			rl.DrawTextEx(rl.GetFontDefault(), str, pos, 34, 2, colors.Blue)
+			pos := util.CenterText(str, float32(textSize40)+2, center)
+			rl.DrawTextEx(rl.GetFontDefault(), str, pos, float32(textSize40)+2, 2, colors.Blue)
 		}
-		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, rl.White)
+		rl.DrawText(str, int32(pos.X), int32(pos.Y), int32(textSize40), rl.White)
 	}
 
 	// Statuses
 	if world.status[world.player][Poison] > 0 {
 		str := "Poisoned"
 		center := rl.Vector2{X: halfSize.X, Y: yPositions[3]}
-		pos := util.CenterText(str, 32, center)
-		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, colors.Purple)
+		pos := util.CenterText(str, float32(textSize40), center)
+		rl.DrawText(str, int32(pos.X), int32(pos.Y), int32(textSize40), colors.Purple)
 	}
 	if world.status[world.player][Slow] > 0 {
 		str := "Slowed"
 		center := rl.Vector2{X: halfSize.X, Y: yPositions[4]}
-		pos := util.CenterText(str, 32, center)
-		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, colors.Blue)
+		pos := util.CenterText(str, float32(textSize40), center)
+		rl.DrawText(str, int32(pos.X), int32(pos.Y), int32(textSize40), colors.Blue)
 	}
 	if world.status[world.player][Bleed] > 0 {
 		str := "Bleeding"
 		center := rl.Vector2{X: halfSize.X, Y: yPositions[5]}
-		pos := util.CenterText(str, 32, center)
-		rl.DrawText(str, int32(pos.X), int32(pos.Y), 32, colors.Red)
+		pos := util.CenterText(str, float32(textSize40), center)
+		rl.DrawText(str, int32(pos.X), int32(pos.Y), int32(textSize40), colors.Red)
 	}
 
 	{ // Boss Bar
@@ -85,7 +85,7 @@ func renderHUD(world *World) {
 		if boss != -1 {
 			hp := world.hp[Entity(boss)]
 			width := int32(size.X * hp.val / hp.max)
-			rl.DrawRectangle(0, int32(size.Y-16), width, 16, colors.Red)
+			rl.DrawRectangle(0, int32(size.Y-16*uiScale), width, int32(16*uiScale), colors.Red)
 		}
 	}
 
--- internal/spells.go
+++ internal/spells.go
@@ -9,8 +9,8 @@ import (
 
 func renderSpells(world *World) {
 	halfHeight := float32(rl.GetScreenHeight() / 2)
-	size := rl.Vector2{X: 200, Y: 80}
-	pos := rl.Vector2{X: 300, Y: halfHeight}
+	size := rl.Vector2{X: 200 * uiScale, Y: 80 * uiScale}
+	pos := rl.Vector2{X: 300 * uiScale, Y: halfHeight}
 
 	spellCount := 3
 	if world.playerData.upgradeCount() > 10 {
@@ -29,8 +29,8 @@ func renderSpells(world *World) {
 		world.playerData.mana -= 5
 	}
 	raygui.Enable()
-	util.DrawTextureCenteredScaled(assets.textures["heal_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-	util.DrawTextCentered("1", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+	util.DrawTextureCenteredScaled(assets.textures["heal_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4*uiScale)
+	util.DrawTextCentered("1", float32(textSize40), rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 
 	// Doll Summon
 	if world.playerData.mana < 10 || world.playerData.dollToSpawn != nil {
@@ -41,8 +41,8 @@ func renderSpells(world *World) {
 		spawnDollWithAnimation(world, &dollTypes.basicDoll)
 		world.playerData.mana -= 10
 	}
-	util.DrawTextureCenteredScaled(assets.textures["doll_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-	util.DrawTextCentered("2", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+	util.DrawTextureCenteredScaled(assets.textures["doll_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4*uiScale)
+	util.DrawTextCentered("2", float32(textSize40), rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 
 	// Upgrade
 	if world.playerData.mana >= 10 {
@@ -54,8 +54,8 @@ func renderSpells(world *World) {
 		world.playerData.mana -= 10
 		newUpgradeScreen(world)
 	}
-	util.DrawTextureCenteredScaled(assets.textures["pitem_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-	util.DrawTextCentered("3", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+	util.DrawTextureCenteredScaled(assets.textures["pitem_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4*uiScale)
+	util.DrawTextCentered("3", float32(textSize40), rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 
 	// Unique Upgrade
 	uniqueUpgradeCost := float32(50)
@@ -73,8 +73,8 @@ func renderSpells(world *World) {
 			world.playerData.mana -= uniqueUpgradeCost
 			newUniqueUpgradeScreen(world)
 		}
-		util.DrawTextureCenteredScaled(assets.textures["unique_upgrade_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4)
-		util.DrawTextCentered("4", 40, rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
+		util.DrawTextureCenteredScaled(assets.textures["unique_upgrade_icon"], rl.Vector2{X: pos.X - size.X/5, Y: pos.Y}, 4*uiScale)
+		util.DrawTextCentered("4", float32(textSize40), rl.Vector2{X: pos.X + size.X/5, Y: pos.Y})
 	}
 
 	raygui.Enable()
