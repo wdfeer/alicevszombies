0e1acb375fb5b6ee1c8d4f58e5fb97876a4bac16
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-22 08:22:57 +0200

    Render all pixel shadows before rendering all pixel particles

--- internal/particle.go
+++ internal/particle.go
@@ -16,6 +16,23 @@ type PixelParticle struct {
 }
 
 func renderPixelParticles(world *World) {
+	if options.Shadows {
+		for id, eff := range world.pixelParticle {
+			var alpha float32
+			switch eff.alphaMode {
+			case 0:
+				alpha = min(eff.timeleft, 1)
+			case 1:
+				alpha = max(1-eff.timeleft, 0)
+			case 2:
+				alpha = 1
+			}
+
+			pos := rl.Vector2Add(world.position[id], rl.Vector2{X: 0.5, Y: 0.5})
+			rl.DrawRectangleV(pos, rl.Vector2{X: 1, Y: 1}, rl.ColorAlpha(rl.Black, alpha))
+		}
+	}
+
 	for id, eff := range world.pixelParticle {
 		var alpha float32
 		switch eff.alphaMode {
@@ -25,11 +42,6 @@ func renderPixelParticles(world *World) {
 			alpha = max(1-eff.timeleft, 0)
 		case 2:
 			alpha = 1
-		}
-
-		if options.Shadows {
-			pos := rl.Vector2Add(world.position[id], rl.Vector2{X: 0.5, Y: 0.5})
-			rl.DrawRectangleV(pos, rl.Vector2{X: 1, Y: 1}, rl.ColorAlpha(rl.Black, alpha))
 		}
 
 		tint := eff.tint
